0f771e91e31504c3a0d0ecf9823c0cc108bd5ee4
Minys <minys@foxmail.com>
2019-09-19 21:38:18 +0800

    [upd] cddd baseline performance

--- multiseq2seq/baseline/cddd_encode_evaluation.py
+++ multiseq2seq/baseline/cddd_encode_evaluation.py
@@ -5,6 +5,9 @@ import os
 import pandas as pd
 from sklearn.svm import SVC, SVR
 from sklearn.model_selection import cross_val_score, LeaveOneGroupOut
+from sklearn.metrics import roc_auc_score
+from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 """
 Simple wrapper for cddd, to extract discriptors.
@@ -101,17 +104,49 @@ def cddd_encode(args):
     print('Done!')
 
 
-def load_cddd_csv(fpath, label_col):
+def load_cddd_csv(fpath, label_col, shuffle=True):
     df = pd.read_csv(fpath).dropna()
     emb_cols = [f"cddd_{i+1}" for i in range(512)]
     smiles = df['new_smiles'].to_numpy()
     valid_idx = df.iloc[:, 0].to_numpy()
     embeddings = df.loc[:, emb_cols].to_numpy()
     labels = df[label_col].to_numpy()
+    assert len(smiles) == len(valid_idx) == len(embeddings) == len(labels)
+    if shuffle:
+        idx = np.random.permutation(len(valid_idx))
+        smiles = smiles[idx]
+        valid_idx = valid_idx[idx]
+        embeddings = embeddings[idx]
+        labels = labels[idx]
     return smiles, valid_idx, embeddings, labels
 
 
+def _check_normalize(features, labels):
+    assert features.shape[0] == labels.shape[0]
+    normalize = StandardScaler()
+    normalize.fit(features)
+    features = normalize.transform(features)
+    return features, labels
+
+
+def evaluation(clf, features, labels, scorefn=None, cv=5):
+    """
+    Evaluate the cddd fingerprints with support vector machines.
+    :param Union[SVC, SVR] clf: estimator object implementing ‘fit’, according to sklearn's estimator object.
+    :param np.ndarray features: The data to fit, shape (n_samples, n_features)
+    :param np.ndarray labels: The label corresponds to `features`, shape (n_samples)
+    :param scorefn: a score function `fn(estimator, x, y) -> score`, higher, the better
+    :param int cv: Cross-validation split, default 5.
+    :return: Raw scores of evalutaion, for each cross-validation.
+    """
+    features, labels = _check_normalize(features, labels)
+    scores = cross_val_score(clf, features, labels, cv=cv, scoring=scorefn)
+    return scores
+
+
 if __name__ == "__main__":
+
+
     # encode
     flist = [
         qsardir / 'classification' / 'bace' / 'bace-cddd.csv',
@@ -130,30 +165,65 @@ if __name__ == "__main__":
         args = parser.parse_args()
         cddd_encode(args)
     # evaluation
+    print(f"Method\tDataset\tMetrics\tMean(CV)\tStd(CV)")
     # classification
+
+    def auc_score(e, x, y):
+        p = e.predict_proba(x)[:, 1]
+        return roc_auc_score(y, p)
+
+    def print_class_result(dataset, auc_roc, acc):
+        print(f"Class.\t{dataset}\tAUC-ROC\t{np.mean(auc_roc):.3f}\t{np.std(auc_roc):.3f}")
+        print(f"Class.\t{dataset}\taccu.\t{np.mean(acc):.3f}\t{np.std(acc):.3f}")
     # bace
     smiles, valid_idx, embeddings, labels = load_cddd_csv(flist[0], label_col='Class')
-    print(smiles.shape, valid_idx.shape, embeddings.shape, labels.shape)
+    auc_roc = evaluation(SVC(probability=True, gamma='scale'), embeddings, labels, scorefn=auc_score)
+    acc = evaluation(SVC(probability=True, gamma='scale'), embeddings, labels, scorefn=None)
+    print_class_result('bace', auc_roc, acc)
+
     # bbbp
     smiles, valid_idx, embeddings, labels = load_cddd_csv(flist[1], label_col='p_np')
-    print(smiles.shape, valid_idx.shape, embeddings.shape, labels.shape)
+    auc_roc = evaluation(SVC(probability=True, gamma='scale'), embeddings, labels, scorefn=auc_score)
+    acc = evaluation(SVC(probability=True, gamma='scale'), embeddings, labels, scorefn=None)
+    print_class_result('bbbp', auc_roc, acc)
+
     # pcba
     # TODO: figure out how to use pcba data, there are too many labels!
+
     # ames
     smiles, valid_idx, embeddings, labels = load_cddd_csv(flist[3], label_col='Activity')
-    print(smiles.shape, valid_idx.shape, embeddings.shape, labels.shape)
+    auc_roc = evaluation(SVC(probability=True, gamma='scale'), embeddings, labels, scorefn=auc_score)
+    acc = evaluation(SVC(probability=True, gamma='scale'), embeddings, labels, scorefn=None)
+    print_class_result('ames', auc_roc, acc)
 
+    # ===========================
     # regression
+    def mse_score(e, x, y):
+        p = e.predict(x)
+        return np.mean((p-y)**2)
+
+    def print_regress_result(dataset, mse, r2):
+        print(f"Regres.\t{dataset}\tMSE\t{np.mean(mse):.3f}\t{np.std(mse):.3f}")
+        print(f"Regres.\t{dataset}\tR^2\t{np.mean(r2):.3f}\t{np.std(r2):.3f}")
+
     # ESOL
     smiles, valid_idx, embeddings, labels = load_cddd_csv(flist[4],
                                                           label_col='ESOL predicted log solubility in mols per litre')
-    print(smiles.shape, valid_idx.shape, embeddings.shape, labels.shape)
+    mse = evaluation(SVR(gamma='scale'), embeddings, labels, scorefn=mse_score)
+    r2 = evaluation(SVR(gamma='scale'), embeddings, labels, scorefn=None)
+    print_regress_result("ESOL", mse, r2)
+
     # lipophilicity
     smiles, valid_idx, embeddings, labels = load_cddd_csv(flist[5], label_col='exp')
-    print(smiles.shape, valid_idx.shape, embeddings.shape, labels.shape)
+    mse = evaluation(SVR(gamma='scale'), embeddings, labels, scorefn=mse_score)
+    r2 = evaluation(SVR(gamma='scale'), embeddings, labels, scorefn=None)
+    print_regress_result("lipo", mse, r2)
+
     # FreeSolv
     smiles, valid_idx, embeddings, labels = load_cddd_csv(flist[6], label_col='expt')
-    print(smiles.shape, valid_idx.shape, embeddings.shape, labels.shape)
+    mse = evaluation(SVR(gamma='scale'), embeddings, labels, scorefn=mse_score)
+    r2 = evaluation(SVR(gamma='scale'), embeddings, labels, scorefn=None)
+    print_regress_result("FreeSolv", mse, r2)
 
 
 
